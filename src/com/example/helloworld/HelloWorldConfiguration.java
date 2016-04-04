f52be61127494db196ff7317ce43718e465dc276
Jasbir <jkalra@digitalriver.com>
2016-04-04 14:50:11 +0530

    second successfull commit

--- src/com/example/helloworld/HelloWorldConfiguration.java
+++ src/com/example/helloworld/HelloWorldConfiguration.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
 import org.hibernate.validator.constraints.NotEmpty;
 
+/**
+ * 
+ * @author jasbirkaurk
+ *
+ */
 public class HelloWorldConfiguration extends Configuration {
 	@NotEmpty
 	@JsonProperty
