0ca15dcd4e5290002f2397ea0693ffad4236531d
Jasbir <jkalra@digitalriver.com>
2016-04-04 14:42:43 +0530

    first successfull commit

---
+++ src/com/example/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.config.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class HelloWorldConfiguration extends Configuration {
+	@NotEmpty
+	@JsonProperty
+	private String template;
+
+	@NotEmpty
+	@JsonProperty
+	private String defaultName = "Stfbfgdfgranger";
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public String getDefaultName() {
+		return defaultName;
+	}
+}
